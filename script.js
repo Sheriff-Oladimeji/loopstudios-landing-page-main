1495dd1860ffa55a2be6f562467d1df93b50d2b9
sheriff oladimeji <dimejiademola5@gmail.com>
2023-01-28 07:28:18 +0100

    modified files

---
+++ script.js
@@ -0,0 +1,4 @@
+const menuIcon = document.querySelector('#menu-icon')
+const closeIcon = document.querySelector('#close-icon')
+const hamburger = document.querySelector('.hamburger')
+const menu = document.querySelector('menu')
