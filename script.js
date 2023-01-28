3ab0cfd8a849353361cd459a7a83ab733d9ed07e
sheriff oladimeji <dimejiademola5@gmail.com>
2023-01-28 10:24:33 +0100

    modified files

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const menuIcon = document.querySelector('#menu-icon')
 const closeIcon = document.querySelector('#close-icon')
 const hamburger = document.querySelector('.hamburger')
-const menu = document.querySelector('menu')
+const menu = document.querySelector('.menu')
+const nav = document.querySelector('nav')
+hamburger.addEventListener('click', mobileNav)
+function mobileNav(){
+  if(nav.classList.contains('showNav')){
+  nav.classList.remove('showNav')
+  menu.style.display = "none"
+  closeIcon.style.display = "none"
+  menuIcon.style.display = "block"
+  }
+  else{
+    nav.classList.add('showNav')
+    menu.style.display = "block"
+    closeIcon.style.display = "block"
+  menuIcon.style.display = "none"
+  }
+}
